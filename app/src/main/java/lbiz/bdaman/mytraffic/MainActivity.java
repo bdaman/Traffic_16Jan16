5f53006b9870e5dd628db848efa0530d7b295fff
Chanunyu <chanunyu.eam@gmail.com>
2016-01-16 14:59:10 +0700

    Button Sound Controller

--- app/src/main/java/lbiz/bdaman/mytraffic/MainActivity.java
+++ app/src/main/java/lbiz/bdaman/mytraffic/MainActivity.java
@@ -1,7 +1,9 @@
 package lbiz.bdaman.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -19,7 +21,25 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
     }   //Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.phonton2);
+                buttonMediaPlayer.start();
+
+
+            }
+        });
+    }   //Button Controller
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.livTraffic);
