d92c83995164160009ba6841fb3141c6bb86d2fe
Chanunyu <chanunyu.eam@gmail.com>
2016-01-17 15:01:26 +0700

    Success Project

---
+++ app/src/main/java/lbiz/bdaman/mytraffic/SplashScreen.java
@@ -0,0 +1,32 @@
+package lbiz.bdaman.mytraffic;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Sound Effect
+        MediaPlayer introMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        introMediaPlayer.start();
+
+        //Thread
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        }, 8000);
+
+    }   //Main Method
+
+}//Main Class
